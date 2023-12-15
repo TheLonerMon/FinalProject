6348295e7e6a3a61bf9d80816c1a613498134111
TheLonerMon <143550154+TheLonerMon@users.noreply.github.com>
2023-12-15 16:38:49 -0500

    Complete Final Project
    Tried to do make  expand a modal, didn't work out the way i wanted. Therefore expand brings you to a new tab.

---
+++ script.js
@@ -0,0 +1,120 @@
+document.getElementById('playButtonRep').addEventListener('click', function () {
+    var iframe = document.querySelector('.overlayRep iframe');
+    iframe.src="https://www.youtube.com/embed/qkTmn1O6uMA?autoplay=1";
+    document.getElementById('overlayRep').style.display = 'flex';
+
+});
+
+document.getElementById('close-button-rep').addEventListener('click', function () {
+    document.getElementById('overlayRep').style.display = 'none';
+    document.querySelector('.overlayRep iframe').src = "";
+});
+// ---------------------------------------------------------------------
+document.getElementById('playButtonMam').addEventListener('click', function () {
+    var iframe = document.querySelector('.overlayMam iframe');
+    iframe.src = "https://www.youtube.com/embed/YrvIsEWOous?autoplay=1";
+    document.getElementById('overlayMam').style.display = 'flex';
+
+});
+
+document.getElementById('close-button-mam').addEventListener('click', function () {
+    document.getElementById('overlayMam').style.display = 'none';
+    document.querySelector('.overlayMam iframe').src = "";
+});
+// ------------------------------------------------------------------------
+document.getElementById('playButtonBir').addEventListener('click', function () {
+    var iframe = document.querySelector('.overlayBir iframe');
+    iframe.src = "https://www.youtube.com/embed/y2lq4IGD7sY?autoplay=1";
+    document.getElementById('overlayBir').style.display = 'flex';
+
+});
+
+document.getElementById('close-button-bir').addEventListener('click', function () {
+    document.getElementById('overlayBir').style.display = 'none';
+    document.querySelector('.overlayBir iframe').src = "";
+});
+// ---------------------------------------------------------------------------
+document.getElementById('playButtonAmp').addEventListener('click', function () {
+    var iframe = document.querySelector('.overlayAmp iframe');
+    iframe.src = "https://www.youtube.com/embed/YM9ZytyxYBU?autoplay=1";
+    document.getElementById('overlayAmp').style.display = 'flex';
+
+});
+
+document.getElementById('close-button-amp').addEventListener('click', function () {
+    document.getElementById('overlayAmp').style.display = 'none';
+    document.querySelector('.overlayAmp iframe').src = "";
+});
+// ---------------------------------------------------------------------------------
+document.getElementById('playButtonFis').addEventListener('click', function () {
+    var iframe = document.querySelector('.overlayFis iframe');
+    iframe.src = "https://www.youtube.com/embed/HJajBtZnWAk?autoplay=1";
+    document.getElementById('overlayFis').style.display = 'flex';
+
+});
+
+document.getElementById('close-button-fis').addEventListener('click', function () {
+    document.getElementById('overlayFis').style.display = 'none';
+    document.querySelector('.overlayFis iframe').src = "";
+});
+// --------------------------------------------------------------------------
+document.getElementById('playButtonIns').addEventListener('click', function () {
+    var iframe = document.querySelector('.overlayIns iframe');
+    iframe.src = "https://www.youtube.com/embed/k3hy3Pq0RH4?autoplay=1";
+    document.getElementById('overlayIns').style.display = 'flex';
+
+});
+
+document.getElementById('close-button-ins').addEventListener('click', function () {
+    document.getElementById('overlayIns').style.display = 'none';
+    document.querySelector('.overlayIns iframe').src = "";
+});
+// --------------------------------------------------------------------------------
+document.getElementById('playButtonAra').addEventListener('click', function () {
+    var iframe = document.querySelector('.overlayAra iframe');
+    iframe.src = "https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1";
+    document.getElementById('overlayAra').style.display = 'flex';
+
+});
+
+document.getElementById('close-button-ara').addEventListener('click', function () {
+    document.getElementById('overlayAra').style.display = 'none';
+    document.querySelector('.overlayAra iframe').src = "";
+});
+// -----------------------------------------------------------------------------------
+document.getElementById('playButtonMol').addEventListener('click', function () {
+    var iframe = document.querySelector('.overlayMol iframe');
+    iframe.src = "https://www.youtube.com/embed/G_GJH1t7VVM?autoplay=1";
+    document.getElementById('overlayMol').style.display = 'flex';
+
+});
+
+document.getElementById('close-button-mol').addEventListener('click', function () {
+    document.getElementById('overlayMol').style.display = 'none';
+    document.querySelector('.overlayMol iframe').src = "";
+});
+// -----------------------------------------------------------------------------------
+document.getElementById('playButtonCni').addEventListener('click', function () {
+    var iframe = document.querySelector('.overlayCni iframe');
+    iframe.src = "https://www.youtube.com/embed/k146Jk7Qk1k?autoplay=1";
+    document.getElementById('overlayCni').style.display = 'flex';
+
+});
+
+document.getElementById('close-button-cni').addEventListener('click', function () {
+    document.getElementById('overlayCni').style.display = 'none';
+    document.querySelector('.overlayCni iframe').src = "";
+});
+// -----------------------------------------------------------------------------------
+document.getElementById('playButtonCru').addEventListener('click', function () {
+    var iframe = document.querySelector('.overlayCru iframe');
+    iframe.src = "https://www.youtube.com/embed/nfTQUX-GMjc?autoplay=1";
+    document.getElementById('overlayCru').style.display = 'flex';
+
+});
+
+document.getElementById('close-button-cru').addEventListener('click', function () {
+    document.getElementById('overlayCru').style.display = 'none';
+    document.querySelector('.overlayCru iframe').src = "";
+});
+// -----------------------------------------------------------------------------------
